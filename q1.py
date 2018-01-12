3ed98f1fe7fbda5dbeee9d978321dde2a42b5272
colman423 <colman0423@gmail.com>
2018-01-12 13:02:48 +0800

    fix bug

--- q1.py
+++ q1.py
@@ -19,10 +19,12 @@ def run(fileName):
     for x in xrange(FILE_COUNT):
         with open(convertToPathStr(x)+".q1hsv") as f:
             lines = map(int, f.read().splitlines())
-            combine = list(map(lambda x: abs(x[0]-x[1]), zip(np.array(lines), fileHistogram)))
+            combine = [abs(item[0]-item[1]) for item in zip(np.array(lines), fileHistogram)]
             for y in xrange(360):
                 if combine[y] > 180:
                     combine[y] = (360 - combine[y])*2
+                else:
+                    combine[y] *= 2
             for y in range(360, 460):
                 combine[y] = combine[y]/2
             compareDistance[x] = np.linalg.norm(combine)
@@ -48,14 +50,24 @@ def offlineProcess(i):
     Arr = [pixel[x, y] for x in xrange(width) for y in xrange(height)]
     Hsv = [colorsys.rgb_to_hsv(item[0]/255., item[1]/255., item[2]/255.) for item in Arr]
 
-    for x in xrange(len(Hsv)):
-        Hsv[x] = (Hsv[x][0]*360, Hsv[x][1]*100, Hsv[x][2]*100)
-        Hadjust = int(round(Hsv[x][0]))
-        if Hadjust==360:
+    # for x in xrange(len(Hsv)):
+    #     Hsv[x] = (Hsv[x][0]*360, Hsv[x][1]*100, Hsv[x][2]*100)
+    #     Hadjust = int(round(Hsv[x][0]))
+    #     if Hadjust==360:
+    #         Hadjust = 0
+    #     hArr[Hadjust] += 1
+    #     sArr[int(round(Hsv[x][1]))] += 1
+    #     vArr[int(round(Hsv[x][2]))] += 1
+
+#136
+    for item in Hsv:
+        Hadjust = int(item[0]*360+0.5)
+        if Hadjust == 360:
             Hadjust = 0
         hArr[Hadjust] += 1
-        sArr[int(round(Hsv[x][1]))] += 1
-        vArr[int(round(Hsv[x][2]))] += 1
+        sArr[int(item[1]*100+0.5)] += 1
+        vArr[int(item[2]*100+0.5)] += 1
+
 
     hsv = open(fileName+".q1hsv", "w")
     for i in hArr+sArr+vArr:
@@ -63,5 +75,8 @@ def offlineProcess(i):
     hsv.close()
 
 if __name__ == "__main__":
+    import time
+    t = time.time()
     for x in range(0, FILE_COUNT):
         offlineProcess(x)
+    print time.time()-t
